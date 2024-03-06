85366579d87ac6e63a29e88019b768a6ad101d0c
BobHuangNCREETW <95524691+BobHuangNCREETW@users.noreply.github.com>
2024-03-06 11:23:10 +0800

    Update sub_pre_FinalModel.m
    Updated coefficients during the revision process on 2024.03.06

--- sub_pre_FinalModel.m
+++ sub_pre_FinalModel.m
@@ -1,15 +1,15 @@
 function [pre_S4_Asp,pre_S3_Area]=sub_pre_FinalModel(Mw,dip,Ztor,NZbor,Evtlon,Evtlat)
 %%%%%%
-%      Source scaling relation submitted to the Seismological Research Letter 2024.
-%                         by Bob J.Y. Huang 2024.01.08
+%      Source scaling relation 
+%                         by Bob J.Y. Huang 2024.03.05
 %%%%%%
 % c0: Constant for M<MBP; c1: slope of M>MBP; 
 % MBP: break magnitude for fault rupture reach the bottom of seismogenic zone, which is dip dependent 
-c0=0.1139; c1=0.523;
+c0=0.1139; c1=0.532;
 % c2; c3 : intercept and slope for dip dependent MBP  
-c2=7.174; c3=-0.011;
-% Coefficient for Stage 3, representing depth dependent reletions
-c4=0.127;c5=-0.205;
+c2=7.17; c3=-0.0105;
+% Coefficient c4 and c5 are for Stage 3, representing depth dependent reletions
+c4=0.126;c5=-0.196;
 % identify region 
 minLON_CA=-125;maxLON_CA=-112;minLAT_CA=31;maxLAT_CA=42;
 minLON_TW=120;maxLON_TW=123;minLAT_TW=21.7;maxLAT_TW=25.5;
@@ -34,9 +34,10 @@ elseif(Mw>MBP_ASPS2) % Mw larger than MBP
 end
 % predicted aspect ratio in S3 model
 pre_S3_Asp=10^(log10(pre_S2_Asp)+c4+(c5*NZbor));
-% subregion "others" is for other model using earthquakes out of the three subregions
-%RTlarge_CA_S3=0.1746;RTlarge_TW_S3=0.0265;RTlarge_JP_S3=-0.0037;RTlarge_ALL_S3=-0.0363;
-RTlarge_CA_S3=0.17;RTlarge_TW_S3=0.03;RTlarge_JP_S3=0;RTlarge_others_S3=-0.04; % use 0 for JP is because the original mean residual is smaller than standard error value
+% subregion "others" is for other regions using earthquakes out of the three subregions
+%RTlarge_CA_S3=0.212;RTlarge_TW_S3=0.0438;RTlarge_JP_S3=0.0085;RTlarge_ALL_S3=-0.0336;
+%  standard error value of CA=0.0619; TW=0.0529; JP=0.0455; others=0.0343
+RTlarge_CA_S3=0.21;RTlarge_TW_S3=0;RTlarge_JP_S3=0;RTlarge_others_S3=0; % use 0 for TW, JP and other is because the mean residuals are smaller than their standard error values
 if(region_flag==1) %region_flag: 1=CA; 2=TW; 3=JP; 4=other;
   RT_dip_c6=RTlarge_CA_S3;
 elseif(region_flag==2)
@@ -58,20 +59,21 @@ elseif(Mw>=MBP_ASPS2+smooth_M_range)
 end
 %%%%%
 % Area scaling
-d0=-0.207;d1=-0.068;d2=1.387;
+d0=-0.215;d1=-0.068;d2=1.371;
 base_model_logA=Mw-4; % use typical constant 4 as a base model in S1_A(M)
 MBP_AreaS2=MBP_ASPS2;
 MBPL_AreaS2=MBP_ASPS2+d2;
 pre_S2_Area=base_model_logA+max([d0*(max([Mw,MBP_AreaS2])-MBPL_AreaS2)+d1,d1]);
 pre_S2_Area=10^pre_S2_Area;
 % sur: surface rupture; bur: buried rupture; devided by Ztor = 1 km 
-d3_AreaS3_sur=-0.237;d4_AreaS3_sur=0.405;
-d3_AreaS3_bur=-0.260;d4_AreaS3_bur=0.295;
+d3_AreaS3_sur=-0.248;d4_AreaS3_sur=0.408;
+d3_AreaS3_bur=-0.269;d4_AreaS3_bur=0.296;
 if(Ztor<1); % surface rupture fault
   pre_S3_Area=10^(log10(pre_S2_Area)+(d3_AreaS3_sur+d4_AreaS3_sur*NZbor));
 elseif(Ztor>=1); % buried fault
   pre_S3_Area=10^(log10(pre_S2_Area)+(d3_AreaS3_bur+d4_AreaS3_bur*NZbor));
 end
+
 
 
 
